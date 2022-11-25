461178b85765f8e4aeb9fcb9bc1713b3f6572cc5
FukadaYuya <fukada.y24@gmail.com>
2022-11-26 02:54:40 +0900

    コンポーネント仮置き

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,5 @@
+import styles from './index.module.css';
+
+export const Button = () => {
+  return <div className={styles.main}>test</div>;
+};
---
+++ src/components/molecules/Header/index.tsx
@@ -0,0 +1,7 @@
+import styles from './index.module.scss';
+
+export const Header = () => {
+  return <header className={styles.main}>
+    Fust Gaming
+  </header>;
+};
