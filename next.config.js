2e275e44f66a48ab59d60af38ee1259b7a750f13
FukadaYuya <fukada.y24@gmail.com>
2023-01-16 04:10:47 +0900

    👍 config修正

--- next.config.js
+++ next.config.js
@@ -6,11 +6,4 @@ const nextConfig = {
 
 module.exports = {
   ...nextConfig,
-  webpack: (config, { webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/));
-    return config;
-  },
 };
