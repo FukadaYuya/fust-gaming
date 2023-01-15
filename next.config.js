93f4f7bd9c100a0d17c09d8c419783926e269f04
FukadaYuya <fukada.y24@gmail.com>
2023-01-16 04:07:53 +0900

    👍 vercel設定追加

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/));
+    return config;
+  },
+};
