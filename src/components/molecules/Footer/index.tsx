e576360122a70316015bb1069163695ab030faf2
FukadaYuya <fukada.y24@gmail.com>
2023-01-16 07:41:50 +0900

    👍 Footer修正

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,7 +1,27 @@
 import styles from './index.module.scss';
+import Link from 'next/link';
 
 export const Footer = () => {
-  return <header className={styles.main}>
-    Footer
-  </header>;
+  return (
+    <footer className={styles.module}>
+      <div className={styles.logo}></div>
+      <nav className={styles.nav}>
+        <ul>
+          <li className={styles.link}>
+            <Link href=".">TOP</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/about">ABOUT</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/member">MEMBER</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/news">GAMES</Link>
+          </li>
+        </ul>
+      </nav>
+      <p className={styles.copyright}>©2023 Fust gaming</p>
+    </footer>
+  );
 };
